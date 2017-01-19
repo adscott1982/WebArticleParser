8a6f87627406d66ae7415c2844fab15db18ca5c9
Andrew Scott <adscott1982@googlemail.com>
2017-01-19 23:03:52 +0000

    First commit

---
+++ WebArticleParser/WebPageText.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Linq;
+
+namespace WebArticleParser
+{
+    public class WebPageText
+    {
+        private HtmlDocument htmlDoc;
+        public string Text { get; private set; }
+
+        public WebPageText(string url)
+        {
+            LoadWebPage(url);
+            LoadText();
+            Text = HtmlEntity.DeEntitize(Text);
+        }
+
+        private void LoadText()
+        {
+            var title = this.htmlDoc.DocumentNode.Descendants("title");
+            if (title.Count() > 0) this.Text += title.First().InnerText + Environment.NewLine;
+            
+            var paragraphs = this.htmlDoc.DocumentNode.Descendants("p");
+
+            foreach (var paragraph in paragraphs)
+            {
+                var isValid = !string.IsNullOrWhiteSpace(paragraph.InnerText) && paragraph.InnerText.Contains(" ");
+                var text = isValid ? "  " + paragraph.InnerText + Environment.NewLine + Environment.NewLine : "";
+                this.Text += text;
+            }
+        }
+
+        private void LoadWebPage(string url)
+        {
+            var web = new HtmlWeb();
+            this.htmlDoc = web.Load(url);
+        }
+    }
+}
---
+++ WebParserComLine/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebArticleParser;
+
+namespace WebParserComLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = @"https://medium.com/defiant/the-young-pope-makes-catholicism-great-again-5631f234604d#.7octnmz13";
+
+            Console.WriteLine($"Loading {url}");
+            Console.WriteLine("Text is:\n");
+
+            var webPage = new WebPageText(url);
+            Console.WriteLine(webPage.Text);
+
+            Console.ReadKey();
+        }
+    }
+}
