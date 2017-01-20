d69f9cc30841c71cfe67e1a8be2b23d1301fb50b
ascott <ascott@visionrt.com>
2017-01-20 16:52:05 +0000

    Removed escape characters from paragraph string and transferred validation logic into LINQ statement over node descendants.

--- WebArticleParser/WebPageText.cs
+++ WebArticleParser/WebPageText.cs
@@ -19,16 +19,27 @@ namespace WebArticleParser
         private void LoadText()
         {
             var title = this.htmlDoc.DocumentNode.Descendants("title");
-            if (title.Count() > 0) this.Text += title.First().InnerText + Environment.NewLine;
+            if (title.Count() > 0) this.Text += title.First().InnerText + Environment.NewLine + Environment.NewLine;
             
-            var paragraphs = this.htmlDoc.DocumentNode.Descendants("p");
+            var paragraphs = this.htmlDoc.DocumentNode.Descendants("p")
+                                                      .Select(n => n.InnerText)
+                                                      .Where(t => !string.IsNullOrWhiteSpace(t))
+                                                      .Select(s => this.RemoveEscapeCharacters(s));
 
             foreach (var paragraph in paragraphs)
             {
-                var isValid = !string.IsNullOrWhiteSpace(paragraph.InnerText) && paragraph.InnerText.Contains(" ");
-                var text = isValid ? "  " + paragraph.InnerText + Environment.NewLine + Environment.NewLine : "";
+                var isValid = paragraph.Contains(" ");
+                var text = isValid ? paragraph + Environment.NewLine + Environment.NewLine : "";
                 this.Text += text;
             }
+        }
+
+        private string RemoveEscapeCharacters(string s)
+        {
+            s = s.Replace("\n", "");
+            s = s.Replace("\t", "");
+
+            return s;
         }
 
         private void LoadWebPage(string url)
